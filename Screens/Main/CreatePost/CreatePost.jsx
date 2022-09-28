5775ad7ecb4c5b4fea59b22a244bdb786c39cb11
Nazar <karpeko76@gmail.com>
2022-09-28 17:52:44 +0100

    upd

--- Screens/Main/CreatePost/CreatePost.jsx
+++ Screens/Main/CreatePost/CreatePost.jsx
@@ -4,27 +4,45 @@ import {
   StyleSheet,
   TextInput,
   TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
 
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
+import HideWithKeyboard from "react-native-hide-with-keyboard";
 
 import TakePhoto from "../../../shared/Components/TakePhoto";
 
 const CreatePost = () => {
+  console.log(Keyboard)
   return (
-    <View style={styles.mainBlock}>
-      <TakePhoto mainBlockStyle={styles.takePhotoBlock} />
-      <View style={styles.form}>
-        <TextInput placeholder="Name" />
-        <TextInput placeholder="Location" />
-        <TouchableOpacity>
-          <Text>Publish</Text>
-        </TouchableOpacity>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.mainBlock}>
+        <HideWithKeyboard>
+          <TakePhoto mainBlockStyle={styles.takePhotoBlock} />
+        </HideWithKeyboard>
+        {/* <KeyboardAvoidingView
+          // style={styles.form}
+          behavior={Platform.OS == "ios" && "padding"}
+        > */}
+          <View style={styles.form}>
+          <TextInput style={styles.input} placeholder="Title..." />
+          <TextInput
+            style={{ ...styles.input, marginBottom: 32 }}
+            placeholder="Location..."
+          />
+          <TouchableOpacity style={styles.publishBtn} activeOpacity={0.6}>
+            <Text style={{ fontSize: 16, color: "#fff" }}>Publish</Text>
+          </TouchableOpacity>
+          </View>
+        {/* </KeyboardAvoidingView> */}
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -39,8 +57,27 @@ const styles = StyleSheet.create({
   },
 
   takePhotoBlock: {
-    height: 267,
-    height: wp("61.5%") + 27,
-    marginBottom: 33,
+    height: 260,
+    marginBottom: 48,
+  },
+
+  input: {
+    height: 45,
+    fontSize: 16,
+    color: "#212121",
+    // padding: 15
+    marginBottom: 23,
+    borderBottomWidth: 1,
+    borderBottomColor: "#E8E8E8",
+  },
+
+  publishBtn: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: 50,
+    borderRadius: 100,
+    backgroundColor: "#FF6C00",
   },
 });
+ 
