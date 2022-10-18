ed6bf3835dc51bab89c3e166b9916e51545be17c
Nazar <karpeko76@gmail.com>
2022-10-18 23:25:10 +0100

    upd

--- Screens/Main/Posts/Posts.jsx
+++ Screens/Main/Posts/Posts.jsx
@@ -1,19 +1,52 @@
-import { Text, View, Image, StyleSheet, ScrollView } from "react-native";
+import { Text, View, Image, StyleSheet, FlatList } from "react-native";
+
+import * as Location from "expo-location";
+
+import POSTS_DB from "./posts.json";
 
 const Posts = () => {
-  return (
-    <ScrollView style={{ paddingVertical: 25, paddingHorizontal: 16 }}>
-      <View style={styles.userBlock}>
-        <Image
-          style={styles.userAvatar}
-          source={require("../../../assets/myAvatar.jpg")}
-        />
-        <View>
-          <Text style={styles.userBlockLogin}>TopTester1337</Text>
-          <Text style={styles.userBlockEmail}>qwerty228@gmail.com</Text>
-        </View>
+  //
+  
+  const renderItem = async({ item }) => {
+    let locationString = "";
+    const location = await Location.reverseGeocodeAsync({
+      longitude: item.coords.longitude,
+      latitude: item.coords.latitude,
+    });
+    locationString = `${location.street}, ${location.city}, ${location.country}`;
+
+    console.log(locationString);
+    return (
+      <View>
+        <Image source={item.photoAddress} />
+        <Text>{item.postName}</Text>
+        <Text>{item.creationDate}</Text>
+        <Text>{item.comments.length}</Text>
+        {/* <Text>{`${location.street}, ${location.city}, ${location.country}`}</Text> */}
       </View>
-    </ScrollView>
+    );
+  };
+
+  return (
+    <FlatList
+      style={{ paddingVertical: 25, paddingHorizontal: 16 }}
+      ListHeaderComponent={
+        <View style={styles.userBlock}>
+          <Image
+            style={styles.userAvatar}
+            source={require("../../../assets/myAvatar.jpg")}
+          />
+          <View>
+            <Text style={styles.userBlockLogin}>TopTester1337</Text>
+            <Text style={styles.userBlockEmail}>qwerty228@gmail.com</Text>
+          </View>
+        </View>
+      }
+      data={POSTS_DB.Posts}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id}
+      // extraData={selectedId}
+    ></FlatList>
   );
 };
 export default Posts;
